4a4199417731087fcc62a967b8cd92c3d1cdb601
Kinsley Tran <kins.tran@comcast.net>
2017-05-11 10:16:36 -0700

    Readme update:

--- yScaleLogChart.js
+++ yScaleLogChart.js
@@ -6,22 +6,26 @@ var chartWrapper = d3.select('#my-div')
                 .call(myChart); 
 */
 
+// https://bl.ocks.org/mbostock/3883245 and https://bl.ocks.org/curran/66d926fe73211fd650ec used heavily
+
 function yScaleLogChart() {
   // Set defaults
   var margin = {
-      top: 40,
-      right: 10,
-      bottom: 10,
-      left: 10
+      top: 20,
+      right: 20,
+      bottom: 30,
+      left: 50
   }
-  var width = 800; 
-  var height = 800; 
+  var width = 900; 
+  var height = 560; 
+  var x = d3.scaleTime().rangeRound([0, width]);
+  var y = d3.scaleLog().rangeRound([height, 0]);
   var colorScale = d3.scaleOrdinal(d3.schemeCategory10) // Sets to preset scale from D3 as default
 
   function myChart(selection) { // selection = element, data = dataset
     selection.each(function(data) {
       var svg = d3.select(this).selectAll("svg").data([data])
-
+      
     })
   }
 
@@ -54,14 +58,6 @@ function yScaleLogChart() {
       return colorScale.range();
     }
     colorScale.range(value);
-    return myChart
-  }
-
-  myChart.categories = function(value) {
-    if(!arguments.length) {
-      return colorScale.domain();
-    }
-    colorScale.domain(value);
     return myChart
   }
 
